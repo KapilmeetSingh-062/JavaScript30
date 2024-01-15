a75c38f405d23073c6e548198ed8939fafeaeb44
Kapilmeet Singh <kapilmeetsingh15@gmail.com>
2024-01-15 06:00:13 -0700

    added play/pause toggle functionality in video player

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,37 @@
+// Get DOM Elements
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+
+// Build functions
+function togglePlay() {
+  const method = video.paused ? "play" : "pause";
+  video[method]();
+
+  //   if (video.paused) {
+  //     video.play();
+  //   } else {
+  //     video.pause();
+  //   }
+}
+
+function updateButton() {
+  const icon = this.paused ? "►" : "❚ ❚";
+  toggle.textContent = icon;
+  //   if (video.paused) {
+  //     toggle.innerHTML = "►";
+  //   } else {
+  //     toggle.innerHTML = "❚ ❚";
+  //   }
+}
+
+// Hook up the event listeners
+
+toggle.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
